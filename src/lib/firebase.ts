d84f16a3bebbafb8ccc8e0b3820af1463bc65869
Rotty_brain Id <rottybrainid@gmail.com>
2025-08-25 04:05:47 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,14 +3,15 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  projectId: "habithelper-9o371",
-  appId: "1:140507692145:web:37f29c86ff391f3ec66fd0",
-  storageBucket: "habithelper-9o371.firebasestorage.app",
   apiKey: "AIzaSyASvbApR6BP1T0g_ySTZWkxEdkVfsW_yiY",
   authDomain: "habithelper-9o371.firebaseapp.com",
-  measurementId: "",
-  messagingSenderId: "140507692145"
+  projectId: "habithelper-9o371",
+  storageBucket: "habithelper-9o371.appspot.com",
+  messagingSenderId: "140507692145",
+  appId: "1:140507692145:web:37f29c86ff391f3ec66fd0",
+  measurementId: "G-XXXXXXXXXX"
 };
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
