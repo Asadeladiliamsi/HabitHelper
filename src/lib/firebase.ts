f9d520e59fec73e97c8efe5b501eee0a3ce54d23
Rotty_brain Id <rottybrainid@gmail.com>
2025-08-25 04:09:40 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
   projectId: "habithelper-9o371",
   storageBucket: "habithelper-9o371.appspot.com",
   messagingSenderId: "140507692145",
-  appId: "1:140507692145:web:37f29c86ff391f3ec66fd0"
+  appId: "1:140507692145:web:37f29c86ff391f3ec66fd0",
 };
 
 
