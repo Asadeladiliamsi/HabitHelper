4102b8e456c1a0183f2c7e1edc509291e7ee575d
Rotty_brain Id <rottybrainid@gmail.com>
2025-08-26 13:55:07 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/guru/layout.tsx
+++ src/app/guru/layout.tsx
@@ -1,9 +1,11 @@
-import AppLayout from "@/app/(app)/layout";
+import AppLayout from '@/app/(app)/layout';
 
 export default function GuruLayout({
   children,
+  params,
 }: {
   children: React.ReactNode;
+  params: { [key: string]: string };
 }) {
   return <AppLayout>{children}</AppLayout>;
 }
--- src/app/siswa/layout.tsx
+++ src/app/siswa/layout.tsx
@@ -1,9 +1,11 @@
-import AppLayout from "@/app/(app)/layout";
+import AppLayout from '@/app/(app)/layout';
 
 export default function SiswaLayout({
   children,
+  params,
 }: {
   children: React.ReactNode;
+  params: { [key: string]: string };
 }) {
   return <AppLayout>{children}</AppLayout>;
 }
