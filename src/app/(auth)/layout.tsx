3e2833170cf01e671b25f7f28abd361f1fe69360
Rotty_brain Id <rottybrainid@gmail.com>
2025-08-28 10:29:18 +0000

    Error: params are being enumerated. `params` should be unwrapped with `R

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,10 +1,13 @@
+
 import { Logo } from '@/components/icons/logo';
 import Link from 'next/link';
 
 export default function AuthLayout({
   children,
+  params: _params,
 }: {
   children: React.ReactNode;
+  params: any;
 }) {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-background p-4">
